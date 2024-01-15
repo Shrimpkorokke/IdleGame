d1a84e91751e120daa31561812fee29525bc733f
Shrimpkorokke <101083615+Shrimpkorokke@users.noreply.github.com>
2024-01-15 16:59:37 +0900

    화면 스크롤링 수치 조절

--- Assets/02_Scripts/04_Map/Reposition.cs
+++ Assets/02_Scripts/04_Map/Reposition.cs
@@ -7,9 +7,9 @@ public class Reposition : MonoBehaviour
 {
     private void LateUpdate()
     {
-        if(transform.position.x > - 10)
+        if(transform.position.x > -27.81f)
             return;
         
-        transform.Translate(24, 0, 0, Space.Self);
+        transform.Translate(46.35f, 0, 0, Space.Self);
     }
 }
