100f460987f8f961aa8678be9fb412c621bc5801
Shrimpkorokke <101083615+Shrimpkorokke@users.noreply.github.com>
2024-05-06 14:46:35 +0900

    conflict test

--- Assets/02_Scripts/GitConflictTest.cs
+++ Assets/02_Scripts/GitConflictTest.cs
@@ -7,7 +7,15 @@ public class GitConflictTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // git Desktop 1
+        // git Desktop 2
+        // git Desktop 3
+
+        // git Desktop 14
+
+        // git Desktop 16
+        // git Desktop 17
+        // git Desktop 18
     }
 
     // Update is called once per frame
